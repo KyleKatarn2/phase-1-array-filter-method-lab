af3173c79c86bd423f43a66ee6a9bd4e8be6ec8e
Jonathan Campos <Malvyx5@gmail.com>
2023-05-26 13:42:58 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    const matchingDrivers = drivers.filter(function (driver) {
+        return driver.toLowerCase() === string.toLowerCase();
+    });
+    return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, string) {
+    const matchingDrivers = drivers.filter(function (driver) {
+        return driver.substring(0, string.length) === string; 
+    });
+    return matchingDrivers;
+}
+
+function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(function (driver) {
+        return driver.name === name;
+    });
+    return matchingDrivers;
+}
